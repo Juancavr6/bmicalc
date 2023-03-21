d65cc128819e5c43cec462dc7e1701dd713763f4
Juan Carlos Vergara <juancavergara6@gmail.com>
2023-03-21 21:59:09 +0100

    Añadido proyecto Cucumber

---
+++ hellocucumber/src/test/java/hellocucumber/escenario1StepDefinitions.java
@@ -0,0 +1,29 @@
+package hellocucumber;
+import io.cucumber.java.en.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import bmicalc.BMICalcImpl;
+
+
+public class escenario1StepDefinitions {
+	double resultado;
+	@Given("Un usuario que desea calcular su BMI")
+	   public void un_usuario_que_desea_calcular_su_BMI() {
+		resultado=0;
+	   }
+	@When("Ingresa su peso y altura en la aplicación Calculadora BMI.")
+	   public void ingresa_peso_altura_validos() {
+		BMICalcImpl calc = new BMICalcImpl();
+		
+			 resultado= calc.bmi(0, 0);
+			
+		
+	   }
+	@Then("La aplicación realiza correctamente el cálculo del BMI utilizando la fórmula correspondiente")
+	   public void devuelve_resultado() {
+		assertEquals(resultado,25);
+	   }
+	
+
+}
---
+++ hellocucumber/src/test/resources/hellocucumber/escenario1.feature
@@ -0,0 +1,6 @@
+Feature: Calculadora BMI
+
+  Scenario: Cálculo del BMI
+    Given Un usuario que desea calcular su BMI.
+    When  Ingresa su peso y altura en la aplicación Calculadora BMI.
+    Then La aplicación realiza correctamente el cálculo del BMI utilizando la fórmula correspondiente
